87206eb29d2afffdc068595d1571ba59c195d6a8
Nehuen Villavicencio <villavicencionehuen@gmail.com>
2026-01-17 04:28:33 -0300

    push final config absolute url no proxy

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://gaston-backendmicroservice.toooti.easypanel.host',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  },
   base: '/',
 })
